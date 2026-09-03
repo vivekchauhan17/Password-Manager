466c4facdd3b56f5bc3b97327e8954c39fbfd847
vivekchauhan17 <chauhanvivek9639@gmail.com>
2026-09-03 21:23:18 +0530

    Some Fixes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,17 +8,10 @@ const Navbar = () => {
         <div className='logo font-bold text-white '>
           <span className='text-green-500'>&lt;</span>
           Pass
-          <span className='text-green-500'>OP/&gt;</span>
+          <span className='text-green-500'>Safe/&gt;</span>
 
         </div>
-        <ul>
-          {/* <li className='flex gap-4'>
-            <a className='hover:font-bold' href="#">Home</a>
-            <a className='hover:font-bold' href="#">About</a>
-            <a className='hover:font-bold' href="#">Contact</a>
 
-          </li> */}
-        </ul>
         <button className='text-white my-5 bg-green-700 rounded-full flex justify-between items-center ring-white ring-1 '>
           <img className='invert p-1 w-10 ' src="/icons/github.svg" alt="Github logo" />
           <span className='font-bold px-2' >GitHub</span>
