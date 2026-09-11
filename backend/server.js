9e87ef5cdd70ebe44923ae211f800d6329d6bb8b
vivekchauhan17 <chauhanvivek9639@gmail.com>
2026-09-11 21:14:32 +0530

    Add The Login Page and fixes the design

--- backend/server.js
+++ backend/server.js
@@ -1,88 +1,94 @@
 const express = require('express');
-
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
-const bodyparser = require('body-parser')
-const cors = require('cors')
-const passport = require('./config/passport.js')
-const jwt = require("jsonwebtoken");
+const bodyparser = require('body-parser');
+const cors = require('cors');
 
+const passport = require('./config/passport.js');
+const authRoutes = require('./routes/auth.js')
 
-dotenv.config()
+dotenv.config();
+
 const app = express();
+
+
+// CORS
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],
   credentials: true,
 }));
 
-// Connection URL
+
+// Middleware
+app.use(bodyparser.json());
+app.use(passport.initialize());
+
+
+// MongoDB
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
-
-// Database Name
 const dbName = 'passop';
 const port = 3000;
-app.use(bodyparser.json())
-// app.use(cors())
 
 client.connect();
 
-//get all the passwords
+
+// Google OAuth Routes
+app.use("/Oauth", authRoutes);
+
+
+// Get all passwords
 app.get('/', async (req, res) => {
+
   const db = client.db(dbName);
   const collection = db.collection('Passwords');
+
   const findResult = await collection.find({}).toArray();
-  res.json(findResult)
+
+  res.json(findResult);
 });
 
-//save a password
+
+// Save a password
 app.post('/', async (req, res) => {
-  const password = req.body
+
+  const password = req.body;
+
   const db = client.db(dbName);
   const collection = db.collection('Passwords');
+
   const findResult = await collection.insertOne(password);
-  res.send({ success: true, result: findResult })
+
+  res.send({
+    success: true,
+    result: findResult
+  });
 });
 
-app.get(
-  "/Oauth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-    prompt: "select_account"
-  })
-);
 
-app.get(
-  "/Oauth/google/callback",
-  passport.authenticate("google", { session: false }),
-  (req, res) => {
-
-    const token = jwt.sign(
-      {
-        googleId: req.user.googleId,
-        email: req.user.email,
-        name: req.user.name
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    res.redirect(`http://localhost:5173`);
-  }
-);
-
-// delete a passwprd by id
+// Delete a password
 app.delete('/', async (req, res) => {
-  const password = req.body
+
+  const password = req.body;
+
   const db = client.db(dbName);
   const collection = db.collection('Passwords');
+
   const findResult = await collection.deleteOne(password);
-  res.send({ success: true, result: findResult })
+
+  res.send({
+    success: true,
+    result: findResult
+  });
 });
 
 
-console.log(process.env.MONGO_URI)
+console.log(process.env.MONGO_URI);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
