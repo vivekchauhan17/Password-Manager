97f6d8737e137cd0edcc2ef3405528af7dc3aed5
vivekchauhan17 <chauhanvivek9639@gmail.com>
2026-09-19 11:41:53 +0530

    Final changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,12 @@ function App() {
 
   return (
     <>
-      <Navbar />
-
-      <button onClick={handleLogin}>
-        Login
-      </button>
+        <Navbar />
 
       <div className='bg-green-50 bg-[linear-gradient(to_bottom,#8080800a_1px,transparent_1px),linear-gradient(to_right,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+
         <Manager />
+        
       </div>
 
       <Footer />
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-
 const Navbar = () => {
+
+
+  const handleLogin = () => {
+    window.location.href = "http://localhost:5174";
+  };
+
+
   return (
     <nav className='bg-slate-800 text-white text-2xl '>
       <div className="mycontainer flex justify-between items-center px-4 h-14 py-5">
@@ -13,19 +19,12 @@ const Navbar = () => {
 
         </a>
 
-        {/* <div className='logo font-bold text-white '>
-          <span className='text-green-500'>&lt;</span>
-          Pass
-          <span className='text-green-500'>Safe/&gt;</span>
-
-        </div> */}
-
+        {/* Login Button */} 
         
+        <button onClick={handleLogin} className="rounded-full bg-green-700 px-5 py-2 text-base font-bold text-white ring-1 ring-white transition-all duration-200 hover:bg-green-600 active:scale-95" > Login With Google
+          </button>
 
-        <button className='text-white my-5 bg-green-700 rounded-full flex justify-between items-center ring-white ring-1 '>
-          <img className='invert p-1 w-10 ' src="/icons/github.svg" alt="Github logo" />
-          <span className='font-bold px-2' >GitHub</span>
-        </button>
+
       </div>
     </nav>
   )
